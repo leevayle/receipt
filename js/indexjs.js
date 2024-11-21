973b6accdcb383aa58c3b78cf5e39eed6cb838c3
lee kinanga <lee254kinanga@gmail.com>
2024-11-21 03:37:07 +0300

    first commit

---
+++ js/indexjs.js
@@ -0,0 +1,14 @@
+// setTimeout( ()=>{
+//     document.getElementById('body').style.background = "white";
+// },900);
+
+const ws = new WebSocket("ws://127.0.0.1:7758");
+
+const textMessage = document.getElementById('text-message');
+const submitButton = document.getElementById('submit-message');
+
+submitButton.addEventListener("click", () => {
+  const userMessage = textMessage.value;
+  ws.send(userMessage);
+  textMessage.value = ''; // Clear the input field
+});
